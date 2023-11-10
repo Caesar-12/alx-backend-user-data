28b4ed5a52555bef44251588c5cef02fe6068ce6
Caesar-12 <idowuenoch65@gmail.com>
2023-11-10 02:06:10 +0000

    Authentication template

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Contains auth class"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Manages API authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Determins if authentication is needed"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Creates authorization header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Creates a user"""
+        return None
