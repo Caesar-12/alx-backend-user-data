fed785eb02c33e2b699fadc5f4aad88346473f12
Caesar-12 <idowuenoch65@gmail.com>
2023-11-12 01:50:13 +0000

    authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,19 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Child authentication class"""
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """returns the Base64 part of the
+        Authorization header for a Basic Authentication
+        """
+        auth_h = authorization_header
+        if not auth_h:
+            return None
+        elif not isinstance(auth_h, str):
+            return None
+
+        if auth_h[:6] == "Basic ":
+            return auth_h[6:]
+        else:
+            return None
