17ee26cb91a8f7512f07c1536476f3d655c4416f
Caesar-12 <idowuenoch65@gmail.com>
2023-11-13 23:11:00 +0000

    Base64decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Contains a child class"""
 from api.v1.auth.auth import Auth
+from base64 import b64decode, binascii
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,22 @@ class BasicAuth(Auth):
             return auth_h[6:]
         else:
             return None
+
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """returns the decoded value of a
+        Base64 string base64_authorization_header
+        """
+        auth_h = base64_authorization_header
+        if not auth_h:
+            return None
+        elif not isinstance(auth_h, str):
+            return None
+
+        try:
+            decoded = b64decode(auth_h)
+            return decoded
+        except binascii.Error:
+            return None
+
