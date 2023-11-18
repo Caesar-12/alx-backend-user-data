5bcfff295bb791ba76f57b6d8d3df876cfeb3f24
Caesar-12 <idowuenoch65@gmail.com>
2023-11-18 02:40:01 +0000

    Hashed password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Contains function that hashes password"""
+import bcrypt
+
+
+def _hash_password(passwd):
+    """converts passwords to bytes"""
+    passwd_bytes = passwd.encode('utf-8')
+    salt = bcrypt.gensalt()
+
+    passwd_hash = bcrypt.hashpw(passwd_bytes, salt)
+
+    return passwd_hash
